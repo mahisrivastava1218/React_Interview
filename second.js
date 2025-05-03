b1a24b6adcca8655b5ef659fa75be514cfc86ea6
Mahima <priyanshisrivastava708016@gmail.com>
2025-05-03 21:54:46 +0530

    second commit

---
+++ second.js
@@ -0,0 +1,12 @@
+//Method overriding
+//Super keyword
+//Inheritance in classes
+//class Syntax - create objects
+//Inheritance among constructor function
+//How constructor function leverage it
+//Prototypal Inheritanve
+//Look at some important interesting properties on function
+//Look at a special construcotr function
+//Functions can have properties
+//Create object using a function
+//functions
