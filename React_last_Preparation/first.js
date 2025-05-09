e25c5413406f129e402d6347644839dc47fc9a89
Mahima <priyanshisrivastava708016@gmail.com>
2025-05-09 23:54:06 +0530

    next commit

--- React_last_Preparation/first.js
+++ React_last_Preparation/first.js
@@ -1,3 +1,5 @@
+const axios=require('axios');
+
 //API Contract & Mock data
 //API Contract contains
 //Api endpoint //request format //response format
@@ -34,9 +36,48 @@
 //Order of Results: the result array maintains the order of input promises 
 //Error Handling: if even a single promise rejects,Promise.all() immediately rejects with that reason
 
+async function fetchData(){
+try{
 
-const [pageData, blogPostsData, reviewsData] = await Promise.all([
-    axios.post("https://jsonplaceholder.typicode.com/posts"),
-    axios.post("https://jsonplaceholder.typicode.com/users"),
-    axios.post("https://jsonplaceholder.typicode.com/comments")
-])
+    const [pageData, blogPostsData, reviewsData] = await Promise.all([
+        axios.post("https://jsonplaceholder.typicode.com/posts"),
+        axios.post("https://jsonplaceholder.typicode.com/users"),
+        axios.post("https://jsonplaceholder.typicode.com/comments")
+    ])
+    console.log(pageData.data, blogPostsData.data, reviewsData.data);
+
+}catch(e){
+        console.error(e);
+}
+}
+fetchData();
+// Axios is used because it simplifies and enhances the process of making HTTP requests in JavaScript, particularly in web development. 
+// It provides a more user-friendly API than the native Fetch API, offering features like automatic JSON parsing, built-in error handling, 
+// interceptors, and request cancellation. These features streamline development and make it easier to handle complex HTTP requests
+
+// It’s great that Promise.all() gave us a boost in the performance.
+// But, as we know, even if a single API call fails, the control would still go to catch block directly. E.g. suppose the posts data is fetched successfully, but the users data API endpoint has some issue. Now, in this case, we would like to at least show the posts as we have received that data. But, this is not possible with Promise.all().
+// Is there a better approach?
+// Promise.allSettled(iterable);
+// iterable: An iterable object (e.g., an array) containing Promises.
+// Exactly same syntax as Promises.all() (just mention allSettled instead of all,
+// Settle Status: Each result will be an object with:
+// status: Either 'fulfilled' or 'rejected'.
+// value: The resolved value (if fulfilled).
+// reason: The rejection reason (if rejected).
+// Error Handling: Unlike Promise.all(), it does not short-circuit on a rejected promise; instead, it waits for all promises to settle.
+// Use Cases: Useful when you want to know the outcome of multiple asynchronous operations, regardless of whether they succeed or fail.
+
+
+
+
+// Material UI helps to create layouts and UI easily
+// Material UI Grid helps to create a Grid layout easily for responsiveness
+
+//every single letter triggering in particular API,it then cost server, it make a longterm impact in backend so debouncing concept is introduced=
+// a small delay after every single character type so that API call only happening when the delay has passed. 
+
+//Lifting State Up
+//If multiple components needs access to the same changing data-> lift the shared state to their closest common ancestor
+
+// debounceSearch(event, debounceTimeout) function to be called on the onChange() event of the search bar
