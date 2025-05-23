338025657bbc33f936d6dbc1de04b34db653883b
Mahima <priyanshisrivastava708016@gmail.com>
2025-05-23 23:53:21 +0530

    carousel

---
+++ React_last_Preparation/Carousel.js
@@ -0,0 +1,8 @@
+// Carousel
+// Carousel allows the users to view a group of content (in our case, Album Cards) from start
+//  to end or even cycle through it(i.e. end is again connected to the start).
+
+// Swiper React Components
+//   npm i swiper
+// import {Swiper} from "swiper/react";
+// import 'swiper/swiper-bundle.min.css';
