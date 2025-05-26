074c3e786d46ab9fd5419e919b6cef2031d904ff
Mahima <priyanshisrivastava708016@gmail.com>
2025-05-26 23:54:12 +0530

    interview questions

--- React_last_Preparation/Interview_Question.js
+++ React_last_Preparation/Interview_Question.js
@@ -104,3 +104,10 @@
 //     </div>
 //   );
 // }
+
+
+// how to prepare for interview?
+// first->// what is the need of it
+// then->// need for it
+// then->// what is it
+// then->// how is it used in real world
